0e663b938747574dc0969025d8524e4787b6759f
Nitish C <86357181+Nitish-bot@users.noreply.github.com>
2026-03-18 02:52:03 +0530

    mandelbrot: finish math and pixel coloring

--- mandelbrot/main.lua
+++ mandelbrot/main.lua
@@ -1,16 +1,89 @@
 require("L5")
 
+local width = 1000.
+local height = 1000.
+
 function setup()
-  size(400, 400)
+    size(width, height)
 
-  -- Set the program title
-  windowTitle("Basic sketch")
+    -- Set the program title
+    windowTitle("Basic sketch")
+    describe('Draws a yellow background')
 
-  describe('Draws a yellow background')
+    noStroke()
 end
 
 function draw()
-  -- Fills the background with the color yellow
-  background(255, 215, 0)
+    drawMandelbrot()
 end
 
+function drawMandelbrot()
+    for x = 0, height do
+        for y = 0, width do
+            local c = pixelToPoint(x, y)
+            local isIn, iter = table.unpack(calculatePointInSet(c))
+            if isIn then
+                set(x, y, color(0, 0, 0))
+            elseif iter > 1 then
+                -- FUN COLORING: Math-based RGB banding
+                -- We multiply 'iter' by different prime-ish numbers to create offset color cycles.
+                -- The '% 255' ensures the colors wrap around instead of maxing out to white.
+                local r = (iter * 13) % 255
+                local g = (iter * 7) % 255
+                local b = (iter * 23) % 255
+
+                set(x, y, color(r, g, b))
+            else
+                set(x, y, color(55, 55, 55))
+            end
+        end
+    end
+
+    updatePixels()
+end
+
+function pixelToPoint(x, y)
+    -- Normalize between [-width/2, width/2] and [-height/2, height/2]
+    local x1 = x - width / 2
+    local y1 = y - height / 2
+
+    -- Noramlize to [-2, 2] range
+    local x2 = x1 * 4 / width
+    local y2 = y1 * 4 / height
+
+    return {
+        x = x2,
+        y = y2,
+    }
+end
+
+function calculatePointInSet(c)
+    -- x represents the real part, y represents the imaginary part
+    local z = { x = 0, y = 0 }
+    -- Mathematically proven that any point greater than
+    -- distance 2 will diverge to infinity
+    local bounds = 2
+    local isIn = true
+
+    local iter = 0
+    while iter < 100 do
+        z = {
+            -- z^2 = (zx + zy*i)^2 = zx^2 + zy^2 * i^2 + (2 * zx * zy)i
+            -- z = (zx^2 - zy^2 + cx) + (2 * zx * zy + cy)i
+            x = z.x * z.x - z.y * z.y + c.x,
+            y = 2 * z.x * z.y + c.y
+        }
+
+        if dist(z.x, z.y, 0, 0) > bounds then
+            isIn = false
+            break
+        end
+
+        iter = iter + 1
+    end
+
+    return {
+        isIn,
+        iter,
+    }
+end
