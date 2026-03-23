961c4850701e3b272f08eed1d5f6371356b0a2d0
Nitish C <86357181+Nitish-bot@users.noreply.github.com>
2026-03-23 15:43:07 +0530

    add assets

--- p5soundExp/sketch.js
+++ p5soundExp/sketch.js
@@ -1,5 +1,21 @@
+let song;
+
+function preload() {
+  song = loadSound("./assets/sorry.mp3")
+}
+
 function setup() {
   createCanvas(400, 400);
+  song.playMode("restart");
+  song.play();
+}
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause()
+  } else {
+    song.play()
+  }
 }
 
 function draw() {
